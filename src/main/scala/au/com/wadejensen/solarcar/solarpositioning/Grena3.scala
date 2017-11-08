wadejensen/SolarSimulator

commit acb0ab9d4b680278a09767daea61e77b9ebe25a7

correct vectorised sun_pos

src/main/scala/au/com/wadejensen/solarcar/solarpositioning/Grena3.scala
@@ -4,14 +4,13 @@ import java.time.{Instant, ZoneId, ZonedDateTime}
 import java.util.Calendar
 import java.util.GregorianCalendar
 
-import org.nd4j.linalg.api.ops.impl.accum.MatchCondition
-import org.nd4j.linalg.indexing.BooleanIndexing
-
 //import java.lang.Math._
 
 import org.nd4j.linalg.api.ndarray.INDArray
 import org.nd4j.linalg.factory.Nd4j
 import org.nd4j.linalg.ops.transforms.Transforms._
+import org.nd4j.linalg.api.ops.impl.transforms
+
 import org.nd4s.Implicits._
 import org.nd4j.linalg.indexing.conditions._
 import org.nd4j.linalg.indexing
@@ -140,13 +139,17 @@ object Grena3 {
     val t2 = t.map(calcT2(_)).toNDArray
     val tE = t2 + 1.1574e-5 * deltaT
     val omegaAtE = tE * 0.0172019715
-    val lambda = tE / 1.720279216e-2 - 1.388803 + sin(omegaAtE - 0.06172) * 3.3366e-2 + sin(omegaAtE * 2.0 - 0.1163) * 3.53e-4
+    //val lambda = -1.388803 + 1.720279216e-2 * tE + 3.3366e-2 * math.sin(omegaAtE - 0.06172) + 3.53e-4 * math.sin(2.0 * omegaAtE - 0.1163)
+    val lambda = tE * 1.720279216e-2 - 1.388803 + sin(omegaAtE - 0.06172) * 3.3366e-2 + sin(omegaAtE * 2.0 - 0.1163) * 3.53e-4
     val epsilon = - tE * 6.19e-9 + 4.089567e-1
     val sLambda = sin(lambda)
     val cLambda = cos(lambda)
     val sEpsilon = sin(epsilon)
-    val cEpsilon = sqrt(-sEpsilon * sEpsilon + 1)
-    var alpha = atan2(sLambda * cEpsilon, cLambda)
+    val cEpsilon = sqrt(-(sEpsilon * sEpsilon) + 1)
+    var alpha = atan2(cLambda, sLambda * cEpsilon)
+
+//    var alpha = math.atan2(sLambda * cEpsilon, cLambda)
+//    if (alpha < 0) alpha += 2 * math.Pi
 
     // The following two lines are equivalent to:
     // if (alpha < 0) alpha += 2 * math.Pi
@@ -169,9 +172,6 @@ object Grena3 {
     val sEpsilon0 = sPhi * sDelta + cPhi * cDelta * cH
     val eP = asin(sEpsilon0) - sqrt(-sEpsilon0 * sEpsilon0 + 1) * 4.26e-5
 
-    // convert from ND4J array to JVM
-    //val ePs = for ( i <- 0 until eP.length ) yield eP.getDouble(i)
-
     // The original calculation for deltaRe:
     //    val deltaRe =
     //      if (temperature < -(273) || temperature > 273 || pressure < 0 || pressure > 3000) 0.0
@@ -184,17 +184,15 @@ object Grena3 {
 
     // But we always use a constant known temperature and pressure as an
     // approximation. So the expression simplifies to:
+    val temp3 =
+    sin(eP + (eP + 0.08919) \ 4.26e-5) /
+      cos(eP + (eP + 0.08919) \ 4.26e-5) *
+      (0.08422 * (pressure / 1000)) / (273.0 + temperature)
 
-//    val deltaRe = ePs.map( ep => {
-//      if (ep > 0.0) (0.08422 * (pressure / 1000)) / ((273.0 + temperature) * math.tan(ep + 0.003138 / (ep + 0.08919)))
-//      else 0.0
-//    }).toArray
+    val deltaRe = eP.gt(0.0) * temp3
 
-
-
-//    val z = PI / 2 - eP - deltaRe
-//    toDegrees(z)
-    Nd4j.create(1)
+    val z = - eP - deltaRe + (math.Pi / 2)
+    z * (180 / math.Pi)
   }
 
 }

src/main/scala/au/com/wadejensen/solarcar/Sun.scala
@@ -3,7 +3,10 @@ package au.com.wadejensen.solarcar
 import java.util.{Date, GregorianCalendar}
 
 import au.com.wadejensen.solarcar.solarpositioning.{AzimuthZenithAngle, DeltaT, Grena3, SPA}
+import org.nd4j.linalg.api.buffer.DataBuffer
+import org.nd4j.linalg.api.buffer.util.DataTypeUtil
 import org.nd4j.linalg.api.ndarray.INDArray
+import org.nd4j.nativeblas.NativeOpsHolder
 import org.nd4s.Implicits._
 
 object Sun {
@@ -52,6 +55,7 @@ object Sun {
                       lons: Array[Double],
                       alts: Array[Double]): INDArray = {
 
+    DataTypeUtil.setDTypeForContext(DataBuffer.Type.DOUBLE)
     // Estimate deltaT based on initial time
     var greg = new GregorianCalendar()
     greg.setTimeInMillis( times(0) * 1000 )

src/main/scala/au/com/wadejensen/solarcar/main/StrategyEngineMain.scala
@@ -4,14 +4,12 @@ import java.io.{BufferedWriter, File, FileWriter}
 import java.time._
 import java.time.format.DateTimeFormatter
 
-import au.com.wadejensen.solarcar.model.RaceCourse
 import au.com.wadejensen.solarcar.{Calibrator, GeoMath, StrategyEngine}
 import com.typesafe.config.ConfigFactory
 import org.nd4j.linalg.api.buffer.DataBuffer
 import org.nd4j.linalg.api.buffer.util.DataTypeUtil
 import org.nd4j.linalg.factory.Nd4j
 
-import scala.collection.immutable.HashMap
 
 /**
   * Strategy engine entry point. This main calculates the performance of
@@ -47,6 +45,9 @@ object StrategyEngineMain extends App {
   DataTypeUtil.setDTypeForContext(DataBuffer.Type.DOUBLE)
   println(Nd4j.getExecutioner.getEnvironmentInformation)
   Nd4j.create(1)
+//  DataTypeUtil.setDTypeForContext(DataBuffer.Type.FLOAT)
+//  NativeOpsHolder.getInstance().getDeviceNativeOps().setElementThreshold(100)
+//  NativeOpsHolder.getInstance().getDeviceNativeOps().setTADThreshold(64)
 
   println(s"$cpus logical CPU cores detected.\n" +
     s"Parallelism set to $parallelism")

commit d2a208113f9ce90b5a24ddc91e1c2816199f9efa

fixed bug in solar position serial introduced in acb0ab9d4b680278a09767daea61e77b9ebe25a7

src/main/scala/au/com/wadejensen/solarcar/solarpositioning/Grena3.scala
@@ -75,7 +75,7 @@ object Grena3 {
     var alpha = math.atan2(sLambda * cEpsilon, cLambda)
     if (alpha < 0) alpha += 2 * math.Pi
     val delta = math.asin(sLambda * sEpsilon)
-    var H = 1.7528311 + 6.300388099 * t + math.toRadians(longitude) - alpha
+    var H = 1.7528311 + 6.300388099 * t2 + math.toRadians(longitude) - alpha
     H = ((H + math.Pi) % (2 * math.Pi)) - math.Pi
     if (H < -math.Pi) H += 2 * math.Pi
     val sPhi = math.sin(math.toRadians(latitude))